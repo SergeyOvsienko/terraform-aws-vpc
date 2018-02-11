c4cea70bf908fcefbbf7cb08ea2b182ac385a2dd
Sergey Ovsienko <serg.ovsienko@gmail.com>
2018-02-11 18:17:17 +0200

    /terraform-aws-vpc

---
+++ README.md
@@ -0,0 +1,32 @@
+# terraform-aws / aws_vpc 
+Sample terraform state to management aws pvc
+
+# Requirements (on host that executes module)
+> Terraform v0.11.1
+
+# How to use
+
+Export enviroment variables with AWS credentials:
+```sh
+export AWS_ACCESS_KEY_ID=YOUR_AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=YOUR_AWS_SECRET_ACCESS_KEY
+```
+
+Edit input.auto.tfvars file and set variables:
+```sh
+aws_region="eu-west-1"
+aws_vpc_name = "example-vpc"
+aws_vpc_cidr = "172.16.0.0/12" # Default 10.0.0.0/16
+```
+
+Run terraform:
+```sh
+terraform init
+terraform plan
+terraform apply
+```
+
+To destroy run:
+```sh
+terraform destroy
+```
---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region     = "${var.aws_region}"
+}
+
+data "aws_availability_zones" "az" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.aws_vpc_cidr}"
+  enable_dns_hostnames = true
+  tags {
+    Name = "${var.aws_vpc_name}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags {
+    Name = "${var.aws_vpc_name}-igw"
+  }
+}
+
+resource "aws_subnet" "subnets" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  count = "${length(data.aws_availability_zones.az.names)}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index + 1)}"
+  availability_zone = "${data.aws_availability_zones.az.names[count.index]}"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "${data.aws_availability_zones.az.names[count.index]}-subnet"
+  }
+}
+
+resource "aws_default_route_table" "route_table" {
+    default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw.id}"
+    }
+
+    tags {
+        Name = "${var.aws_vpc_name}-default-rt"
+    }
+}
+
+resource "aws_security_group" "security_group" {
+    name = "${var.aws_vpc_name}-sg"
+    description = "${var.aws_vpc_name}-sg"
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["${var.aws_vpc_cidr}"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    tags {
+        Name = "${var.aws_vpc_name}-sg"
+    }
+}
+
+output "vpc_id" { 
+    value = "${aws_vpc.vpc.id}" 
+}
+
+output "vpc_cidr" { 
+    value = "${aws_vpc.vpc.cidr_block}" 
+}
+
+output "vpc_igw" { 
+    value = "${aws_internet_gateway.igw.id}" 
+}
+
+output "default_rt" {
+    value = "${aws_default_route_table.route_table.id}"
+}
+
